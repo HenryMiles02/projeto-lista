17a4aab07d9b27756f951c2cc55e5c21dabf887b
HenryMiles02 <rosacarlos953@gmail.com>
2025-02-22 17:38:19 -0300

    funcionalidade do alerta e terminação do css

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 const shopInput = document.getElementById('shopInput');
 const shopBtn = document.getElementById('shopBtn');
 const shopList = document.getElementById('shopList');
+const img = document.querySelector('img');
 
-shopBtn.addEventListener("click", function (e) {
-    const listItem = document.createElement('li');
+shopBtn.addEventListener("click", function () {
+  const listItem = document.createElement('li');
     listItem.innerHTML = `
       <input type="checkbox" id="item">
       <span for="none">${shopInput.value}</span>
-      <img src="img/lixeira.png" alt="lixeira">`
-    shopList.appendChild(listItem);
-});
+      `
+  const img = document.createElement('img');
+  img.src = "img/lixeira.png";
+  img.alt = "lixeira";
+  listItem.appendChild(img);
 
+  img.addEventListener("click", function () {
+    listItem.remove()
+  });
+
+    shopList.appendChild(listItem);
+
+    shopInput.value = "";
+
+    const danger = document.getElementById('danger');
+    danger.style = `
+    display: flex;
+    `
+
+    function removeAlert() {
+      setTimeout(function () {
+        danger.style = `
+        display: none;
+        `
+      }, 5000);
+    }
+
+    removeAlert();
+  });
+
+  
